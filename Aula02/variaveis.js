19e3114409776a8e7811d270f3239d655dcb7e71
Davi Tavares <dxrktavares3@gmail.con>
2025-02-20 08:29:03 -0300

    Aula sobre o tipo de variaveis

---
+++ Aula02/variaveis.js
@@ -0,0 +1,20 @@
+var numero = 2;
+let texto = "hello";
+const oQueEisso = "?";
+
+console.log('Tipo var:', numero);
+
+numero = 13;
+
+console.log(texto+ " o numero agora é "+ numero);
+
+console.log(texto, " o numero agora é ",numero);
+
+console.log(`${texto} o numero agora é ${numero}`);
+
+
+texto = 69
+
+console.log(texto + numero);
+
+console.log(oQueEisso);
