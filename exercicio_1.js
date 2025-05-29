b234b86e2081e5d3cc6e65e861267674511280de
Davi Tavares <dxrktavares3@gmail.com>
2025-05-29 08:33:12 -0300

    Exemplos de Ts e Js + Exercicio 1 de ts

---
+++ exercicio_1.js
@@ -0,0 +1,6 @@
+var produto = 'Mouse';
+var preço = 59.90;
+var disponibilidade = true;
+if (disponibilidade = true) {
+    console.log("Produto: ".concat(produto), "- Pre\u00E7o: ".concat(preço), "- Dispon\u00EDvel: Sim");
+}
