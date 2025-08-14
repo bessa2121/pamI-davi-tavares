1150f5dc8028b0f345e9bc81520b704e9e5744c2
Davi Tavares <dxrktavares3@gmail.com>
2025-08-14 08:35:33 -0300

    Atualiza app

--- StickerSmash/app/(tabs)/about.tsx
+++ StickerSmash/app/(tabs)/about.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -12,10 +12,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
-    color: '#000000',
+    color: '#fff',
   },
 });
